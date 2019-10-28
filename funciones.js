3cbd3446a442ff072432601c71ecb340fa63f2b1
Daniel Queralto Parra <queraltoies@gmail.com>
2019-10-28 21:01:12 +0100

    optimizacion
    optimizacion de codigo en  funciones menores

--- funciones.js
+++ funciones.js
@@ -20,18 +20,15 @@ module.exports = {
     {
         switch (comando) {
             case "del"://borra un numero de filas del chat de texto
-                try{
-                    let cantidad = parseInt(args[0])+1;
-                    ms.channel.bulkDelete(cantidad);
-                }catch(err){
-                    console.log(err+"\nInserte el numeró de lineas a borrar");
-                }
+                this.borrado(ms,args[0])//pasamos el primer argumento que es el nº de mendajes a borrar
                 break;
 
             case "ms"://el bot dice algo por ti a alguien
                     try{
+                        this.borrado(ms,0);
+                        let usuario = ms.mentions.members.first();
                         let mensaje = args.join(" ");
-                        ms.channel.send(`**${mencionado.username}**, ${mensaje}`);
+                        ms.channel.send(`**${usuario.username}**, ${mensaje}`);
                     }catch(err){
                         console.log(err+"\n");
                     }
@@ -39,9 +36,14 @@ module.exports = {
 
             case "msp"://manda mensajes privados mediante el bot
                     try{
+                        this.borrado(ms,0);
+                        let usuario = ms.mentions.members.first();
+                        if(!usuario) return ms.channel.send(`indica un usuario.`);
+
                         let mensaje = args.join(" ");
                         if(!mensaje) return ms.channel.send(`Escriba un mensaje para enviartelo por privado.`);
-                        
+
+                        usuario.send(mensaje);
                         ms.author.send(mensaje);
                     }catch(err){
                         console.log(err+"\n ");
@@ -59,6 +61,7 @@ module.exports = {
         switch (comando) {
             case "ban"://baneo, expulsa del server
                 try{
+                    this.borrado(ms,0);
                     let mencionado = ms.mentions.users.first();
                     let razon = args.slice(1).join(' ');
 
@@ -73,49 +76,45 @@ module.exports = {
                 break;
 
             case "mt"://muteo
-                try{
-                    let miembro = ms.mentions.members.first();
-                    let nombrerol = "Muteado";
-
-                    let role = ms.guild.roles.find(r => r.name === nombrerol);
-                    let perms = ms.member.hasPermission("MANAGE_ROLES");
-
-                    if(!perms) return ms.channel.send("No tienes permisos suficientes, para agregar roles.");
-                    if(!miembro) return ms.reply('Debe mencionar a un miembro.');
-                    if(!role) return ms.channel.send('Rol no encontrado en el servidor.');
-
-                    miembro.addRole(role).catch(console.error);
-                    miembro.setMute(true);
-                    
-                }catch(err){
-                    console.log(err.stack);
-                }
+                this.borrado(ms,0);
+                this.mute(ms,true);
                 break;   
 
-                case "dmt"://desmuteo
-                        try{
-                            let miembro = ms.mentions.members.first();
-                            let nombrerol = "Muteado";
-        
-                            let role = ms.guild.roles.find(r => r.name === nombrerol);
-                            let perms = ms.member.hasPermission("MANAGE_ROLES");
-        
-                            if(!perms) return ms.channel.send("No tienes permisos suficientes, para agregar roles.");
-                            if(!miembro) return ms.reply('Debe mencionar a un miembro.');
-                            if(!role) return ms.channel.send('Rol no encontrado en el servidor.');
-        
-                            miembro.removeRole(role).catch(console.error);
-                            miembro.setMute(false);
-                            
-                        }catch(err){
-                            console.log(err.stack);
-                        }
-                        break;   
-
+            case "dmt"://desmuteo
+                this.borrado(ms,0);
+                this.mute(ms,false);
+                break;   
 
             default:
 
                 break;
+        }
+    },
+    mute:function(ms,estado) {
+        try{
+            let miembro = ms.mentions.members.first();
+            let nombrerol = "Muteado";
+
+            let role = ms.guild.roles.find(r => r.name === nombrerol);
+            let perms = ms.member.hasPermission("MANAGE_ROLES");
+
+            if(!perms) return ms.channel.send("No tienes permisos suficientes, para agregar roles.");
+            if(!miembro) return ms.reply('Debe mencionar a un miembro.');
+            if(!role) return ms.channel.send('Rol no encontrado en el servidor.');
+
+            miembro.removeRole(role).catch(console.error);
+            miembro.setMute(estado);
+            
+        }catch(err){
+            console.log(err.stack);
+        }
+    },
+    borrado: function(ms,args=1){
+        try{
+            let cantidad = parseInt(args)+1;
+            ms.channel.bulkDelete(cantidad);
+        }catch(err){
+            console.log(err+"\nInserte el numeró de lineas a borrar");
         }
     }
 

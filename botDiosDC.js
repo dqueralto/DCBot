2fb303c391960e466b84434f11c6c852efa440d8
Daniel Q Parra <queraltoies@gmail.com>
2019-10-28 14:29:48 +0100

    Control por Rol
    Se implementa una la funcion para distinguir el rol de gestion de un usuario ordinario.

--- botDiosDC.js
+++ botDiosDC.js
@@ -11,17 +11,17 @@ var prefix = config.prefix;
 
 function comprobRol() 
 {
-    let rol = message.guild.roles.find(r => r.name === "DIOS");
+    let rol = message.guild.roles.find(r => r.name === "Administrador");
     if(!rol) {
         message.channel.send('Rol no encontrado en el servidor.');
-        return false;
+
     } else{
         if(message.member.roles.has(rol.id)) {
-        ///message.channel.send('Si tienes el rol: `'+rol.name+'`.');
-            return true;
+        message.channel.send('Si tienes el rol: `'+rol.name+'`.');
+
         } else {
-        //message.channel.send('No tienes el rol: `'+rol.name+'`.');
-            return false;
+        message.channel.send('No tienes el rol: `'+rol.name+'`.');
+
         };
     };
 }
@@ -39,25 +39,50 @@ bot.on("message", (message) => {
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
+    
 
-    if (comprobRol) {
-        if (command ==  "ping") {
-            message.channel.send("pong!");
-        } else
-        if (command ==  "hola") {
-            message.channel.send("Hola que tal?");
-        }else
-        if (command ==  "bien") {
-            message.channel.send("Me alegro por ti");
-        }else
-        if (command ==  "mal") {
-            message.channel.send("Pues te jodes");
-        }else
-        if (command ===  "del") {
-            let cantidad = parseInt(args[0]);
-            message.channel.bulkDelete(cantidad);
-        }
-    }
+    let rol = message.guild.roles.find(r => r.name === "DIOS");
+    if(!rol) {
+        message.channel.send('Rol no encontrado en el servidor.');
+
+    } else{
+        if(message.member.roles.has(rol.id)) {
+        //message.channel.send('Si tienes el rol: `'+rol.name+'`.');
+
+            if (command ===  "del") 
+            {
+                try{
+                    let cantidad = parseInt(args[0]);
+                    message.channel.bulkDelete(cantidad);
+                }catch(err){
+                    console.log(err+"\nInserte el numeró de lineas a borrar");
+                }
+            }
+
+            
+
+        } else {
+        //message.channel.send('No tienes el rol: `'+rol.name+'`.');
+
+            if (command ==  "ping") {
+                message.channel.send("pong!");
+            } else
+            if (command ==  "hola") {
+                message.channel.send("Hola que tal?");
+            }else
+            if (command ==  "bien") {
+                message.channel.send("Me alegro por ti");
+            }else
+            if (command ==  "mal") {
+                message.channel.send("Pues te jodes");
+            }
+
+
+
+        };
+    };
+
+
 
 
 
